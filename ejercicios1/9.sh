bb8ed4bf3c4a445eb7d1dfde3c33ba928fd33531
jmenendezasir <jmenendezasir@gmail.com>
2021-11-16 13:08:44 +0000

    16/11

--- ejercicios1/9.sh
+++ ejercicios1/9.sh
@@ -6,11 +6,10 @@ echo "--------------"
 echo " EJERCICIO 9  "
 echo "--------------"
 echo ""
-echo "Vamos a comprimir los ficheros $*"
-for i in "$@"
-do
-	tar -cpvzf copia.bkp $i
-	echo "Fichero $i comprimido correctamente"
-done
+echo "Vamos a comprimir los ficheros de $*"
+
+tar -cpvzf copia.bkp $*
+echo "Los ficheros de $* se han comprimido correctamente"
+
 
 echo "Todos los ficheros se han comprimido en copia.bkp"
