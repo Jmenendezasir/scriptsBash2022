785d697d19f4610717cd9fc4d10e9bedf30108a2
jmenendezasir <jmenendezasir@gmail.com>
2021-11-16 12:49:54 +0000

    16/11

--- ejercicios1/8.sh
+++ ejercicios1/8.sh
@@ -8,12 +8,12 @@ echo "--------------"
 
 if [ -d $1 ]
 then
-    SIZE=`du -sh -h $1`
+    SIZE=`du -sh $1`
     echo "El tamaño del directorio $1"
     echo $SIZE
 elif [ -f $1 ]
 then
-    SIZE=`du -h $1`
+    SIZE=`du -sh $1`
     echo "El tamaño del fichero $1"
     echo $SIZE
 else
