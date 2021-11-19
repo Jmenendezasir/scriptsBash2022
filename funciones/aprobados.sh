6293a559a3103a0458d7ef67c03c2191ba75bad4
jmenendezasir <jmenendezasir@gmail.com>
2021-11-19 08:50:18 +0000

    19/11

---
+++ funciones/aprobados.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Autor: u-server
+# Fecha:  19/11/2021
+# Descripción: Pasar un listado de notas y determinar quién ha aprobado
+
+clear
+aprobados=0
+
+for i in $*
+do
+    if [ $i -gt 10 -o $i -lt 0 ]
+    then
+        echo "Se ha introducido una nota de forma incorrecta."
+        echo "USO: Introducir una nota comprendida entre 0 y 10"
+        exit
+    fi
+    if [ $i -ge 5 ]
+    then
+        let aprobados=aprobados+1
+    sleep .1
+    fi
+done
+
+echo "hay $aprobados alumnos aprobados"
